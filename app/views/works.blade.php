0f878750ffee584b87ee633114f18fba0aeec781
Duncan Smith <duncanssmith@gmail.com>
2014-09-25 20:38:34 +0100

    Image first

--- app/views/works.blade.php
+++ app/views/works.blade.php
@@ -5,22 +5,22 @@
 Works:
     <table>
         <thead>
+            <th>&nbsp;</th>
             <th>Title</th>
             <th>Reference</th>
             <th>Media</th>
             <th>Dimensions</th>
             <th>Date</th>
-            <th>Image</th>
         </thead>
     @foreach($works as $work)
         <tr>
+        <td><img src="/media/images/64/sh_{{ $work->reference }}.jpg"></td>
         <td>{{ $work->title }}</td>
         <td>{{ $work->reference }}</td>
         <td>{{ $work->media }}</td>
         <td>{{ $work->dimensions }}</td>
         <td>{{ $work->work_date }}</td>
 
-        <td><img src="/media/images/64/sh_{{ $work->reference }}.jpg"></td>
         </tr>
     @endforeach
     </table>
