743b280bb71208f05c61c47c1556b31b31ba727c
Duncan Smith <duncanssmith@gmail.com>
2014-10-01 21:32:33 +0100

    Confide generated Users controller, routes and model

--- app/routes.php
+++ app/routes.php
@@ -25,7 +25,7 @@ Route::resource('works', 'WorkController');
 
 Route::resource('groups', 'GroupController');
 
-Route::resource('users', 'UserController');
+//Route::resource('users', 'UserController');
 
 
 
@@ -49,3 +49,16 @@ Route::resource('users', 'UserController');
 //
 //    return View::make('groups')->with('groups', $groups);
 //});
+//
+
+// Confide routes
+Route::get('users/create', 'UsersController@create');
+Route::post('users', 'UsersController@store');
+Route::get('users/login', 'UsersController@login');
+Route::post('users/login', 'UsersController@doLogin');
+Route::get('users/confirm/{code}', 'UsersController@confirm');
+Route::get('users/forgot_password', 'UsersController@forgotPassword');
+Route::post('users/forgot_password', 'UsersController@doForgotPassword');
+Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
+Route::post('users/reset_password', 'UsersController@doResetPassword');
+Route::get('users/logout', 'UsersController@logout');
