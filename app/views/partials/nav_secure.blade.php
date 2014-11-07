eccadde48d202e9850c64df2203e6cbfb13e84d3
Duncan Smith <duncanssmith@gmail.com>
2014-11-07 17:06:56 +0000

    Add icons for secure links

--- app/views/partials/nav_secure.blade.php
+++ app/views/partials/nav_secure.blade.php
@@ -1,7 +1,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="navbar-header">
             <ul class="nav navbar-nav">
-                <li><a href="/publicgroup/17"><i class="fa fa-clock-o icon-white"></i></a></li>
+                <li><a href="/publicgroup/17"><i class="fa fa-clock-o icon-white"></i> Current</a></li>
 
                 @if (Auth::guest())
 
@@ -9,10 +9,10 @@
 
                 @else
 
-                <li><a href="/groups">Groups</a></li>
-                <li><a href="/works">Works</a></li>
-                <li><a href="/texts">Texts</a></li>
-                <li><a href="/users">Users</a></li>
+                <li><a href="/groups"><i class="fa fa-folder icon-white"></i> Groups</a></li>
+                <li><a href="/works"><i class="fa fa-heart icon-white"></i> Works</a></li>
+                <li><a href="/texts"><i class="fa fa-file-text icon-white"></i> Texts</a></li>
+                <li><a href="/users"><i class="fa fa-user icon-white"></i> Users</a></li>
                 <li><a href="/logout">Logout</a></li>
 
                 @endif
