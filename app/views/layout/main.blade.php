860a9fcedf3cdf428dd09daf1d0e7b931762b1a3
Duncan Smith <duncanssmith@gmail.com>
2016-02-05 10:08:55 +0000

    Fix the sortable js which was broken

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -4,23 +4,35 @@
     	<meta charset="UTF-8">
         <title>{{$title}}</title>
 
-        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-        <link rel="stylesheet" href="/media/css/font-awesome.css">
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css">
         <link rel="stylesheet" href="/media/css/main.css">
         <link rel="stylesheet" href="/media/css/carousel.css">
 
-        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="/media/js/vendor/jquery/jquery-2.1.1.min.js"></script>
+
         <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-        <script src="{{ asset('/media/js/vendor/jquery/jquery-2.1.1.min.js') }}"></script>
         <script src="{{ asset('/media/js/vendor/twitter/bootstrap/js/bootstrap.min.js') }}"></script>
         <script src="//cdn.ckeditor.com/4.4.5/basic/ckeditor.js"></script>
         <script src="{{ asset('/media/js/image_sort.js')}}"></script>
         <script src="{{ asset('/media/js/text_sort.js')}}"></script>
         <script src="{{ asset('/media/js/carousel_slider_bootstrap_thumbs.js') }}"></script>
 
-        
     </head>
 
     <body <?php echo isset($body_class) ? 'class="'.$body_class.'"' : ''?>>
