7d5c1c40d994f2cf40154f16a2c63d0dd255fc18
Duncan Smith <duncansmith@Mac.communityfibre.co.uk>
2025-10-12 12:12:06 +0100

    Add exhibitions group to composer

--- app/Acme/View/Composer/NavigationComposer.php
+++ app/Acme/View/Composer/NavigationComposer.php
@@ -2,13 +2,13 @@
 
 class NavigationComposer {
 
-    private array $exhibitions = [];
-	
     public function compose($view)
     {
         $group_list = \Group::where('display', '=', 1)->orderBy('order', 'asc')->get();
+        $exhibition_list = \Group::where('exhibitions', '=', 1)->orderBy('order', 'asc')->get();
 
         $view->with('group_list', $group_list);
+        $view->with('exhibitions_list', $exhibitions_list);
     }
 
 }
